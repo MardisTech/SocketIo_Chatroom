ab01a77eac89f3c508a4ebc730e5860e8e7c48c9
Alex Mardis <mardistech@gmail.com>
2023-08-07 13:57:10 -0400

    set up api routes and chatroom css

---
+++ main.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_socketio import join_room, leave_room, send, SocketIO
+import random
+from string import ascii_uppercase
+
+#Flask basic startup, define the name, update config dictionary value for secret key, and initialize socketIO
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "LAKSJDKF"
+socketio = SocketIO(app)
+
+#Initialize empty rooms dictionary to store generated rooms. function to generate room codes of length
+rooms = {}
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            break
+
+    return code
+
+
+
+
+#flask routing. use decorator @app.route, add allowed methods as list passed to route method, define functions
+@app.route("/", methods=["POST", "GET"])
+def home():
+    session.clear()
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
+        if not name:
+            return render_template("home.html", error="Please enter a name.", code=code, name=name)
+        
+        if join != False and not code:
+            return render_template("home.html", error="Please enter a room code.", code=code, name=name)
+
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages": []}
+        elif code not in rooms:
+            return render_template("home.html", error="Room does not exist.", code=code, name=name)
+        
+        session["room"] = room
+        session["name"] = name
+
+        return redirect(url_for("room"))
+
+
+    return render_template("home.html")
+
+@app.route("/room", methods=["POST", "GET"])
+def room():
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+
+    return render_template("room.html", room=room)
+
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
